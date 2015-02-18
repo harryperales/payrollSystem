e2fef58dc89c94f043667f0d5f7709d9877022f2
june ligan <ligan.june@gmail.com>
2015-02-19 00:14:46 +0800

    update new MiscService
    update new MiscService

--- service/MiscService.cs
+++ service/MiscService.cs
@@ -7,13 +7,13 @@ using PayrollSystem.model;
 
 namespace PayrollSystem.service
 {
-    public class MiscService: MiscServiceInterface
+    public class MiscService : MiscServiceInterface
     {
-        SqlConnection sqlCon = new SqlConnection(@"Data Source=.\SQLEXPRESS;AttachDbFilename=C:\Users\JenuNagil\Documents\GitHub\PayrollSystem\PayrollSystem\Payroll.mdf;Integrated Security=True;User Instance=True");
+        SqlConnection sqlCon = new SqlConnection(@"Data Source=.\SQLEXPRESS;AttachDbFilename=D:\projects\PayrollSystem\PayrollSystem v1.0\PayrollSystem\PayrollSystem\Payroll.mdf;Integrated Security=True;User Instance=True");
         SqlCommand sqlCmd = new SqlCommand();
         SqlDataReader sqlDataReader;
 
-        public MiscService ()
+        public MiscService()
         {
             sqlCmd.Connection = sqlCon;
         }
@@ -30,6 +30,74 @@ namespace PayrollSystem.service
             sqlCon.Close();
             Console.WriteLine(miscellaneous.id);
 
+            return miscellaneous;
+        }
+
+
+        public List<Miscellaneous> fetchAllMisc()
+        {
+            List<Miscellaneous> misc = new List<Miscellaneous>();
+
+            sqlCon.Open();
+            sqlCmd.CommandText = "SELECT [Miscellaneous].id, [Miscellaneous].name, [Miscellaneous].amount, [Miscellaneous].type, [Miscellaneous].description FROM [Miscellaneous]";
+            sqlDataReader = sqlCmd.ExecuteReader();
+            if (sqlDataReader.HasRows)
+            {
+                while (sqlDataReader.Read())
+                {
+                    Miscellaneous miscellaneous = new Miscellaneous();
+
+                    miscellaneous.id = Int32.Parse(sqlDataReader["id"].ToString());
+                    miscellaneous.name = sqlDataReader["name"].ToString();
+                    miscellaneous.amount = Decimal.Parse(sqlDataReader["amount"].ToString());
+                    // miscellaneous.type = MiscType.Parse(sqlDataReader["type"].ToString());
+                    miscellaneous.description = sqlDataReader["description"].ToString();
+                    misc.Add(miscellaneous);
+                }
+            }
+            sqlCon.Close();
+
+            return misc;
+        }
+
+        public Miscellaneous fetchMiscByName(string selectedMisc)
+        {
+            Miscellaneous misc = new Miscellaneous();
+            sqlCon.Open();
+            sqlCmd.CommandText = "SELECT id, name, amount, type, description FROM Miscellaneous "
+                         + "WHERE (name = @name);";
+            sqlCmd.Parameters.AddWithValue("@name", selectedMisc);
+            sqlDataReader = sqlCmd.ExecuteReader();
+            if (sqlDataReader.HasRows)
+            {
+                while (sqlDataReader.Read())
+                {
+                    misc.id = Int32.Parse(sqlDataReader["id"].ToString());
+                    misc.name = sqlDataReader["name"].ToString();
+                    misc.description = sqlDataReader["description"].ToString();
+                    //misc.type = MiscType.Parse(sqlDataReader["type"].ToString()); 
+                    misc.amount = Decimal.Parse(sqlDataReader["amount"].ToString());
+                }
+            }
+            else
+            {
+                Console.WriteLine("nothing");
+            }
+            sqlCon.Close();
+            return misc;
+        }
+
+        public Miscellaneous updateMiscellaneous(Miscellaneous miscellaneous)
+        {
+            sqlCon.Open();
+            sqlCmd.CommandText = "UPDATE [Miscellaneous] SET name = @name, description = @description, amount = @amount, type = @type  WHERE (id = @id)";
+            sqlCmd.Parameters.AddWithValue("@name", miscellaneous.name);
+            sqlCmd.Parameters.AddWithValue("@description", miscellaneous.description);
+            sqlCmd.Parameters.AddWithValue("@amount", miscellaneous.amount);
+            sqlCmd.Parameters.AddWithValue("@type", miscellaneous.type);
+            sqlCmd.Parameters.AddWithValue("@id", miscellaneous.id);
+            sqlCmd.ExecuteNonQuery();
+            sqlCon.Close();
             return miscellaneous;
         }
     }
--- service/MiscServiceInterface.cs
+++ service/MiscServiceInterface.cs
@@ -8,6 +8,12 @@ namespace PayrollSystem.service
 {
     public interface MiscServiceInterface
     {
+        List<Miscellaneous> fetchAllMisc();
+
         Miscellaneous addMisc(Miscellaneous misc);
+
+        Miscellaneous fetchMiscByName(string selectedMisc);
+
+        Miscellaneous updateMiscellaneous(Miscellaneous miscellaneous);
     }
 }
